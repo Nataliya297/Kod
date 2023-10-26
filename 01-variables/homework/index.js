01d9e39deee07bb934a9b33f6c7e340b3b221573
Nataliya297 <148856476+Nataliya297@users.noreply.github.com>
2023-10-26 12:38:54 +0300

    добавила папки

---
+++ 01-variables/homework/index.js
@@ -0,0 +1,17 @@
+/**1 */
+6 > 4 && 10 < 5 // false
+(30 + 4) > (7 ** 2) // false
+null && true// null
+!('hi' + 'hi') && true// false
+!('hi' + 'hi') && true// false
+/**2 */
+// просто сравнение
+// + сравнивается тип данных
+
+/**3 */
+const ten = 10; 
+const hundred = 100;
+const thousand = 1000;
+
+console.log((ten * ten + hundred) * hundred >= thousand * ten )
+//20000>=10000-false
---
+++ 01-variables/lesson/index.js
@@ -0,0 +1,46 @@
+let name = "Nataliya";
+let age = 46;
+let isOpen = true;
+let n = 1;
+let str = "Volga";
+let age2 = null;
+let number = undefined;
+//операторы
+let x = 2;
+let y = 4;
+let res = x>y; //false
+
+let compare = "Светлый" > "Cвет";
+console.loc (compare); //true
+
+let numberString = "Светлый" > 3;
+console.loc (numberString); //false
+
+let a = true;
+let b = false;
+let c = a>b; //true
+
+let a1 = 5;
+let b1 = "svet";
+let c1 = a1<b1; //false
+
+let a2 = 3;
+let b2 = false;
+let c2 = a2>b2; //true
+
+let a3 = "3";
+let b3 = true;
+let c3 = a3>b; //true
+
+let a4 = 5;
+let b4 = null;
+let c4 = a4>b4; //true
+
+let a5 = undefined;
+let b5 = true;
+let c5 = a5 == b5; //false
+
+let a6 = undefined;
+let b6 = null;
+let c6 = a6 == b6; //true
+
