2a9cde3d53c39f095f3a34d77e1f8a819f97dfcc
Nataliya297 <nataliabank@mail.ru>
2023-11-04 00:36:02 +0300

    новая

---
+++ 02-functions/lesson/index.js
@@ -0,0 +1,9 @@
+//1 задание
+const name = "Nataliya";
+const name2 = "Nikita";
+
+if (name === "Nataliya") {
+  console.log(`Приветствую, ${name}`);
+} else {
+  console.log("Вы еще не зарегистрированы, Зарегистрироваться?");
+}
