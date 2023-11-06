7887cf9f954d77f078c99b45428c787ad06208a8
Nataliya297 <nataliabank@mail.ru>
2023-11-06 22:51:32 +0300

    домашнее задание циклы

---
+++ 03-loops/homework/index.js
@@ -0,0 +1,36 @@
+//1
+//Создать цикл, который выводит числа от 1 до 10 включительно.
+console.log("DZ-1");
+for (let count = 1; count <= 10; count++) {
+  console.log(count);
+}
+
+//2
+//Написать цикл, который выводит только четные числа от 1 до 20.
+
+console.log("DZ-2");
+for (let counter = 1; counter <= 20; counter++) {
+  const isEven = counter % 2 === 0;
+  if (isEven) {
+    console.log(counter);
+  }
+}
+
+//3
+//Реализовать цикл, который выводит таблицу умножения для числа 5.
+console.log("DZ-3");
+let five = 5;
+for (let count = 1; count <= 10; count++) {
+  const multiply = five * count;
+  console.log(`${five}*${count}=${multiply}`);
+}
+//4
+//Написать функцию, которая считает сумму всех чисел от 1 до 100.
+console.log("DZ-4");
+let res1 = 0;
+for (counter = 1; counter <= 100; counter++) {
+  const sum = res1 + counter;
+  console.log(`${res1}+${counter}=${sum}`);
+  res1 += counter;
+}
+console.log(`Итого: ${res1}`);
